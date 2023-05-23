d40907e236b88110ea71ff09860d2e5ae2ececd6
Patryk <patrykjanas27@gmail.com>
2023-05-23 18:26:25 +0200

    add thymeleaf

--- src/main/java/com/example/restaurantservice/controller/OrdersController.java
+++ src/main/java/com/example/restaurantservice/controller/OrdersController.java
@@ -1,4 +1,14 @@
 package com.example.restaurantservice.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class OrdersController {
+
+    @GetMapping("/index")
+    public String home(Model model) {
+        return "index";
+    }
 }
