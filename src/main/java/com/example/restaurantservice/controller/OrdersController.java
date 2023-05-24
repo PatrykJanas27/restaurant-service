112beb5f51a8f895a5e72ab049fde9cf5b856d10
Jakub Hałucha <halucha@student.agh.edu.pl>
2023-05-24 18:31:20 +0200

    thymleaf,  css, controller

--- src/main/java/com/example/restaurantservice/controller/OrdersController.java
+++ src/main/java/com/example/restaurantservice/controller/OrdersController.java
@@ -1,14 +1,68 @@
 package com.example.restaurantservice.controller;
 
+import com.example.restaurantservice.dto.CustomerDto;
+import com.example.restaurantservice.entity.Food;
+import com.example.restaurantservice.entity.OrderProduct;
+import com.example.restaurantservice.repository.CustomerRepository;
+import com.example.restaurantservice.repository.FoodRepository;
+import com.example.restaurantservice.repository.OrdersRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
+@AllArgsConstructor
 public class OrdersController {
+    private final FoodRepository foodRepository;
+    private final OrdersRepository ordersRepository;
+    private final CustomerRepository customerRepository;
 
     @GetMapping("/index")
     public String home(Model model) {
+        model.addAttribute("customerDto", customerRepository.findAll());
         return "index";
     }
+
+    @GetMapping("/makeAnOrder")
+    public String showData(Model model) {
+        model.addAttribute("foods", foodRepository.findAll());
+        return "makeAnOrder";
+    }
+
+    @PostMapping("/submitOrder")
+    public String submitOrder(@RequestParam("selectedFoods") List<Integer> selectedFoods, HttpServletRequest request, Model model) {
+        OrderProduct order = new OrderProduct();
+        List<Food> foods = new ArrayList<>();
+        for (Integer id : selectedFoods) {
+            String quantityStr = request.getParameter("quantity" + id);
+            int quantity = Integer.parseInt(quantityStr);
+            Food food = foodRepository.findById(id).orElse(null);
+            if (food != null) {
+                foods.add(food);
+            }
+        }
+        order.setFoods(foods);
+        order.setOrderTotalPrice(order.calculateTotalPrice(foods));
+        // Set other fields of the order...
+        // Save the order to the database
+        ordersRepository.save(order);
+        model.addAttribute("order", order);
+        return "orderConfirmation";
+    }
+
+    @PostMapping("/appointment/save")
+    public String saveAppointment(@ModelAttribute CustomerDto customerDto) {
+        // Save the customerDto...
+        return "redirect:/makeAnOrder";
+    }
 }
+
+
